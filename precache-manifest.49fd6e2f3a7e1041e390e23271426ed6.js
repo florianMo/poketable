5cd7701614b2fc87e807816bba5b653364032768
hugo <hugobqdweb@laposte.net>
2020-04-28 21:15:52 +0200

    Updates

---
+++ precache-manifest.49fd6e2f3a7e1041e390e23271426ed6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0807f04e02ace073366b17a264ac633",
+    "url": "/poketable/index.html"
+  },
+  {
+    "revision": "a6f31a18b548c4f1cc35",
+    "url": "/poketable/static/css/main.3bce0d57.chunk.css"
+  },
+  {
+    "revision": "82c498c17b455b4dd1f3",
+    "url": "/poketable/static/js/2.cd33858c.chunk.js"
+  },
+  {
+    "revision": "512e58aa859c752dc22478474c1d9e1f",
+    "url": "/poketable/static/js/2.cd33858c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6f31a18b548c4f1cc35",
+    "url": "/poketable/static/js/main.9bc46c9a.chunk.js"
+  },
+  {
+    "revision": "9084c669f723b83e8028",
+    "url": "/poketable/static/js/runtime-main.8384eb8e.js"
+  },
+  {
+    "revision": "ddcac4185cc764c07b8435ab0103482e",
+    "url": "/poketable/static/media/bump.ddcac418.mp3"
+  },
+  {
+    "revision": "efa8295f50632f2cceda9a1009ae4fe5",
+    "url": "/poketable/static/media/coin2.efa8295f.mp3"
+  },
+  {
+    "revision": "a5375ff591909131a02c59d744258ccf",
+    "url": "/poketable/static/media/dead.a5375ff5.wav"
+  },
+  {
+    "revision": "eb69c905c327d591a01106e86e35cf79",
+    "url": "/poketable/static/media/tick.eb69c905.mp3"
+  }
+]);
